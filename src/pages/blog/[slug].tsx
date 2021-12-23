92d463932adb6bfa2b277143fa6eacb0053e85a0
asiqueira <36564924+alansiq@users.noreply.github.com>
2021-12-23 13:53:18 -0300

    Cleanup on slug.tsx

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -4,7 +4,6 @@ import { getMDXComponent } from "mdx-bundler/client";
 import { getAllPosts, getSinglePost } from "../../services/BlogPostService";
 import Prism from "prismjs";
 import "prismjs/themes/prism.css";
-import { useRouter } from "next/router";
 
 //@ts-ignore
 const CustomLink = ({ as, href, ...otherProps }) => {
