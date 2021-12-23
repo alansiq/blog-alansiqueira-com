ec472d35a30deeda3c22c81b9c3f2f017ed90e17
asiqueira <36564924+alansiq@users.noreply.github.com>
2021-12-23 02:09:43 -0300

    Added Navbar component

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,71 @@
+import Container from "@mui/material/Container";
+import styled from "@emotion/styled";
+import Link from "next/link";
+
+const Logo = styled.h1`
+  font-size: 1em;
+  font-weight: bolder;
+  color: #fff;
+`;
+
+const LinkList = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+
+  li {
+    cursor: pointer;
+    position: relative;
+
+    padding: 8px;
+
+    &:before {
+      content: "";
+      position: absolute;
+      background: #c32319;
+      height: 2px;
+      width: 0%;
+
+      bottom: 0;
+      left: 50%;
+
+      transition: all 0.2s;
+    }
+
+    &:hover {
+      &:before {
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
+`;
+
+const Nav: React.FC = (props) => {
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "space-between",
+        margin: "48px 0px",
+      }}
+    >
+      <Logo>Alan Siqueira</Logo>
+      <LinkList>
+        <Link passHref href="/">
+          <li>Home</li>
+        </Link>
+        <Link passHref href="/blog">
+          <li>Blog</li>
+        </Link>
+        <Link passHref href="/goodies">
+          <li>Goodies</li>
+        </Link>
+      </LinkList>
+    </Container>
+  );
+};
+
+export default Nav;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 import Layout from "../layouts/DefaultLayout";
+import Nav from "../components/Nav";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -10,6 +12,7 @@ import "@fontsource/roboto/700.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Nav></Nav>
       <Component {...pageProps} />
     </Layout>
   );
